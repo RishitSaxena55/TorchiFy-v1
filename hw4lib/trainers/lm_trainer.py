962d84385c9faccead70cd277cb4f2d49bd88481
Rishit Saxena <rishitsaxena55@gmail.com>
2025-07-19 12:12:45 +0530

    fixed lm_trainer

--- hw4lib/trainers/lm_trainer.py
+++ hw4lib/trainers/lm_trainer.py
@@ -80,9 +80,9 @@ class LMTrainer(BaseTrainer):
             # Unpack batch from the dataloader
             # Move the batch elements to self.device
             targets_shifted, targets_golden, lengths = zip(*batch)
-            targets_shifted = targets_shifted.to(self.device)
-            targets_golden = targets_golden.to(self.device)
-            lengths = lengths.to(self.device)
+            targets_shifted = torch.stack(targets_shifted).to(self.device)
+            targets_golden = torch.stack(targets_golden).to(self.device)
+            lengths = torch.tensor(lengths).to(self.device)
 
             with torch.autocast(device_type=self.device, dtype=torch.float16):
 
@@ -173,9 +173,9 @@ class LMTrainer(BaseTrainer):
             # Unpack batch
             # Move the batch elements to self.device
             targets_shifted, targets_golden, lengths = zip(*batch)
-            targets_shifted = targets_shifted.to(self.device)
-            targets_golden = targets_golden.to(self.device)
-            lengths = lengths.to(self.device)
+            targets_shifted = torch.stack(targets_shifted).to(self.device)
+            targets_golden = torch.stack(targets_golden).to(self.device)
+            lengths = torch.tensor(lengths).to(self.device)
 
             # Forward pass
             with torch.inference_mode():
